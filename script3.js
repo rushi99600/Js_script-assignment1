556be35de6bca32a07cee6692e22b7ce248237d0
RUSHIKESH LAMBTURE <61156884+rushi99600@users.noreply.github.com>
2022-09-18 17:20:03 +0000

    all problems added

--- script3.js
+++ script3.js
@@ -2,23 +2,22 @@
  * Function to populate values in card from form input
  */
 function generateCard() {
-  // Get value of Student name from form input 
-  const nameElement = document.getElementById("name");
+  // Get value of Student name from form input
+  const nameElement = document.getElementById('name');
   const nameValue = nameElement.value;
   // Assign the value of student name to generated card
-  const cardNameElement = document.getElementById("cardName");
+  const cardNameElement = document.getElementById('cardName');
   cardNameElement.innerHTML = nameValue;
 
-  // Get value of college name from form input 
-  const collegeNameValue = document.getElementById("collegeName").value;
+  // Get value of college name from form input
+  const collegeNameValue = document.getElementById('collegeName').value;
   // Assign the value of college name to generated card
-  document.getElementById("cardCollegeName").innerHTML = collegeNameValue;
+  document.getElementById('cardCollegeName').innerHTML = collegeNameValue;
 
-  // Get value of location name from form input 
-  const locationValue = document.getElementById("location").value;
-  // Assign the value of location name to generated card
-  document.getElementById("cardLocation").innerHTML = locationValue;
+  // Get value of location name from form input
+  const locationValue = document.getElementById('location').value;
 
-  // Display final generated card to user     
-  document.getElementById("collegeCard").style.display = "block";
+  document.getElementById('cardLocation').innerHTML = locationValue;
+
+  document.getElementById('collegeCard').style.display = 'block';
 }
