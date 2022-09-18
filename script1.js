798689a3fb534fe745623490ae8dd20fff313f56
RUSHIKESH LAMBTURE <61156884+rushi99600@users.noreply.github.com>
2022-09-18 16:06:24 +0000

    problem2 added

--- script1.js
+++ script1.js
@@ -1,31 +1,21 @@
-function WhatTimeIsIt() {
-  //FOR TIME
-  let clock = new Date();
-  let hour = clock.getHours();
-  if (hour < 10) {
-    hour = '0' + hour;
-  }
-  let mn = clock.getMinutes();
-  if (mn < 10) {
-    mn = '0' + mn;
-  }
-  let sec = clock.getSeconds();
-  if (sec < 10) {
-    sec = '0' + sec;
-  }
-  let timeIs = hour + ' : ' + mn + ' : ' + sec;
-  document.getElementById('time').innerText = timeIs;
-  //For date
-  let day = clock.getDate();
-  if (day < 10) {
-    day = '0' + day;
-  }
-  let month = clock.getMonth() + 1;
-  if (month < 10) {
-    month = '0' + month;
-  }
-  let year = clock.getFullYear();
-  let date = day + ' / ' + month + ' / ' + year;
-  document.getElementById('date').innerText = date;
+function getTime() {
+  var clockTime = new Date().toLocaleTimeString([], {
+    hours: '2-digit',
+    minutes: '2-digit',
+    seconds: '2-digit',
+  });
+  document.getElementById('timeContainer').innerText = clockTime;
 }
-setInterval(WhatTimeIsIt, 1000);
+(function showDate() {
+  var todayDate = new Date();
+  document.getElementById('dateContainer').innerText = todayDate.toDateString();
+})();
+var intervalRef;
+
+function startClock() {
+  intervalRef = setInterval(getTime, 1000);
+}
+
+function stopClock() {
+  clearInterval(intervalRef);
+}
